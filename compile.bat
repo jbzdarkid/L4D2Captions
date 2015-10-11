1b2f26254af89ba44b834645c03440b96ad0ce86
jbzdarkid <jbzdarkid@gmail.com>
2015-10-11 19:41:17 -0400

    Stopped pausing, it was getting annoying.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,4 @@
 for %%f in (closecaption_*.txt) do (
   "%cd%\bin\captioncompiler.exe" "%cd%\%%f"
-  pause
   copy /y "%cd%\%%~nf.dat" "Z:\Users\joe\Library\Application Support\Steam\SteamApps\common\Left 4 Dead 2\left4dead2\resource"
 )
